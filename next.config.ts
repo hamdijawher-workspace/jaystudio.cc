be9e6c28b334d0cbd5b0d0cc0b49215af5766ef2
hamdijawher <hamdijawher@icloud.com>
2026-06-13 02:31:46 +0100

    Fix production assets on nested pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "." : undefined,
   output: "export",
   trailingSlash: true
 };
